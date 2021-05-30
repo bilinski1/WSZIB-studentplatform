7a41ea3150a8baccfdc0b9cddf792ac3c862a6f8
bilinski <abc@example.com>
2021-05-30 22:17:46 +0200

    Initial commit

---
+++ src/test/java/angly/me/studentplatform/StudentplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package angly.me.studentplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
