3713db4faabb4176033a6dd57014545dfe1a3057
bilinski <abc@example.com>
2021-08-11 12:16:07 +0200

    deploy attempt 1

--- src/main/ui/src/services/auth-service.js
+++ src/main/ui/src/services/auth-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8081/students";
+const API_URL = "http://localhost:8080/students";
 
 class AuthService {
   //WORKING
